b6bf4f644277fc71b4c0f060d4565a4cc4058f52
siddiquishahina01-cmyk <siddiquishahina01@gmail.com>
2025-09-06 15:56:39 +0530

    commit

---
+++ story_content/user.js
@@ -0,0 +1,631 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "61evGCisogk":
+        Script1();
+        break;
+      case "65k3wLhiv0S":
+        Script2();
+        break;
+      case "67PqM5RLJJx":
+        Script3();
+        break;
+      case "5wMvrAgTiTI":
+        Script4();
+        break;
+      case "6JNVBiicFZD":
+        Script5();
+        break;
+      case "5pRZ1yxsgqk":
+        Script6();
+        break;
+      case "5iw1Kf4tEwp":
+        Script7();
+        break;
+      case "6Q2v2DSZik7":
+        Script8();
+        break;
+      case "5WHiITx4a7T":
+        Script9();
+        break;
+      case "6Hha9SAsHiK":
+        Script10();
+        break;
+      case "6X4G5ChOSFj":
+        Script11();
+        break;
+      case "6lH1aBrxg4y":
+        Script12();
+        break;
+      case "6NT8h1Sb0Fw":
+        Script13();
+        break;
+      case "5wtoaWOxv1e":
+        Script14();
+        break;
+      case "5d7z4qhJ1aC":
+        Script15();
+        break;
+      case "6jSOc7nXMdD":
+        Script16();
+        break;
+      case "6TBFaOawz82":
+        Script17();
+        break;
+      case "66ZQG9xIlh1":
+        Script18();
+        break;
+      case "5nOBJt4WPJG":
+        Script19();
+        break;
+      case "5ZWvU7JBKLu":
+        Script20();
+        break;
+      case "5yoyZtkU91O":
+        Script21();
+        break;
+      case "5eu2QGzuVqW":
+        Script22();
+        break;
+      case "5uDHtLfbItJ":
+        Script23();
+        break;
+      case "6DmebxkG8Qa":
+        Script24();
+        break;
+      case "6diy4guDvhN":
+        Script25();
+        break;
+      case "5lgSQZeYf3X":
+        Script26();
+        break;
+  }
+}
+
+function Script1()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script2()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script3()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script4()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script5()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script6()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script7()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script8()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script9()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script10()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script11()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script12()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script13()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script14()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script15()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script16()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script17()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script18()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script19()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script20()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script21()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script22()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script23()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script24()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script25()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
+function Script26()
+{
+  function FullScreen(element) 
+{
+    if(element.requestFullScreen) 
+    {
+      element.requestFullScreen();
+    } 
+    else if(element.mozRequestFullScreen) 
+    {
+      element.mozRequestFullScreen();
+    } 
+    else if(element.webkitRequestFullScreen) 
+    {
+      element.webkitRequestFullScreen();
+    }
+}
+  
+FullScreen(document.documentElement);
+}
+
